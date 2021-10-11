627c573b0dc34609ebbe5bdccb9e5129ed946a56
juan sanchez <johhns@hotmail.com>
2021-10-11 15:56:53 -0600

    Initial commit

---
+++ app/src/main/java/com/developer/johhns/http_01/MainActivity.java
@@ -0,0 +1,124 @@
+package com.developer.johhns.http_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText entrada;
+    private TextView salida;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        entrada = (EditText) findViewById(R.id.EditText01);
+        salida = (TextView) findViewById(R.id.TextView01);
+
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.
+                Builder().permitNetwork().build());
+
+    }
+
+    public void buscar(View view){
+        try {
+            String palabras  = entrada.getText().toString();
+            String resultado = resultadosGoogle(palabras);
+            salida.append(palabras + "--" + resultado + "\n");
+        } catch (Exception e) {
+            salida.append("Error al conectar\n");
+            Log.e("HTTP", e.getMessage(), e);
+        }
+    }
+
+
+    public String resultadosGoogle(String palabras) throws Exception {
+        String pagina = "", devuelve = "";
+        URL url = new URL("http://appserver.iea.com.sv/wsag/Sag_HH_Util.Sincronizar_Departamentos");
+/*        URL url = new URL("http://appserver.iea.com.sv/wsag/Sag_HH_Util.Sincronizar_Departamentos"
+                       + URLEncoder.encode(palabras, "UTF-8") + "\"");
+
+* */
+        HttpURLConnection conexion = (HttpURLConnection)  url.openConnection();
+       // conexion.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1)");
+
+        if ( conexion.getResponseCode() == HttpURLConnection.HTTP_OK ) {
+            BufferedReader reader = new BufferedReader( new InputStreamReader( conexion.getInputStream() ) ) ;
+            String linea = reader.readLine() ;
+            while ( linea != null ) {
+                pagina += linea ;
+                linea  =  reader.readLine() ;
+            }
+            reader.close() ;
+            devuelve = pagina ; //buscaAproximadamente(pagina);
+        } else {
+            devuelve = "ERROR: " + conexion.getResponseMessage();
+        }
+        conexion.disconnect();
+        return devuelve;
+    }
+
+    String buscaAproximadamente(String pagina){
+        int ini = pagina.indexOf("Aproximadamente");
+        if ( ini != -1 ) {
+            int fin = pagina.indexOf(" ", ini + 16);
+            return pagina.substring(ini + 16, fin);
+        } else {
+            return "no encontrado";
+        }
+    }
+
+
+    public void buscar2(View view){
+        String palabras = entrada.getText().toString();
+        salida.append(palabras + "--");
+        new BuscarGoogle().execute(palabras);
+    }
+
+    class BuscarGoogle extends AsyncTask<String, Void, String> {
+        private ProgressDialog progreso;
+        @Override protected void onPreExecute() {
+            progreso = new ProgressDialog(MainActivity.this);
+            progreso.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progreso.setMessage("Accediendo a pagina...");
+            progreso.setCancelable(false); // false: no muestra botón cancelar
+            progreso.show();
+        }
+        @Override protected String doInBackground(String... palabras) {
+            try {
+                return resultadosGoogle(palabras[0]);
+            } catch (Exception e) {
+                cancel(false); //true: interrumpimos hilo, false: dejamos termine
+                Log.e("HTTP", e.getMessage(), e);
+                return null;
+            }
+        }
+        @Override protected void onPostExecute(String res) {
+            progreso.dismiss();
+            salida.append(res + "\n");
+        }
+
+        @Override protected void onCancelled() {
+            progreso.dismiss();
+            salida.append("Error al conectar\n");
+        }
+    }
+
+
+
+}
